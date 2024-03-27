24af0bc0a5922b9a62cca4b2483e513a7e32cb5e
yandjoumbi <ydjoumbi@gmail.com>
2024-03-27 18:46:25 -0500

    added custom message

--- terraform-eks/docker/app.py
+++ terraform-eks/docker/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Dockerized Flask App!'
+    return 'Hello, Les amis today c est le 3/27/2024'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
